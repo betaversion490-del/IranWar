f749eb90ac6e0ad8eb8fb0ab4608eb08fec8cd18
Z User <z@container>
2026-07-20 09:37:33 +0000

    07954c4d-5331-45eb-b6f0-4f3206b2f349

--- scripts/package_zip.sh
+++ scripts/package_zip.sh
@@ -5,7 +5,7 @@ set -e
 
 PROJECT_DIR="/home/z/my-project"
 DOWNLOAD_DIR="${PROJECT_DIR}/download"
-VERSION="3.0.0"
+VERSION="3.1.0"
 ZIP_NAME="middle-east-strategic-game-v${VERSION}.zip"
 ZIP_PATH="${DOWNLOAD_DIR}/${ZIP_NAME}"
 
@@ -28,6 +28,7 @@ COPY_PATHS=(
   "mini-services"
   "examples"
   "RESEARCH.md"
+  "GAME_TREE.md"
   "package.json"
   "package-lock.json"
   "bun.lock"
